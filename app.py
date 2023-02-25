33fa9bc7f33f3e0ee0caa9b3519b9293607df2bf
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2023-02-25 14:26:08 +0100

    Add vite (#2)

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 
-app = Flask(__name__, static_folder="app", static_url_path="/app")
+app = Flask(__name__)
 
 
 @app.route("/health")
@@ -8,7 +8,20 @@ def health():
     return jsonify({"status": "OK"})
 
 
+@app.route("/api/hello")
+def api_hello():
+    return jsonify({"message": "Hello World!"})
+
+
+# Static files
+@app.route("/assets/<path:path>")
+def catch_all(path):
+    return send_from_directory("./frontend/dist/assets", path)
+
+
+# TODO: JavaScript stacktraces are not readable
+# TODO: Hot module reloading does not work
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
-def catch_all(path):
-    return app.send_static_file("index.html")
+def index(path):
+    return send_from_directory('./frontend/dist', "index.html")
