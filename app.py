ceed28e4403000850f19d8c1a1575d12097008b9
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2023-02-25 16:03:52 +0100

    Add dash app callback

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import dash as dash
-from dash import html
+from dash import html, dcc, Output, Input
 from flask import Flask, jsonify, send_from_directory
 
 app = Flask(__name__)
@@ -10,7 +10,23 @@ dash_app = dash.Dash(
     url_base_pathname="/dash/"
 )
 
-dash_app.layout = html.Div(id="dash-container", children="Hello Dash!")
+dash_app.layout = html.Div(
+    [html.H6("Change the value in the text box to see callbacks in action!"),
+    html.Div([
+        "Input: ",
+        dcc.Input(id='my-input', value='initial value', type='text')
+    ]),
+    html.Br(),
+    html.Div(id='my-output')],
+)
+
+
+@dash_app.callback(
+    Output(component_id='my-output', component_property='children'),
+    Input(component_id='my-input', component_property='value')
+)
+def update_output_div(input_value):
+    return f'Output: {input_value}'
 
 
 @app.route("/health")
