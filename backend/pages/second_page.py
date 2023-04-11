207cf8c096b063752df2a2466f50d7fc45e2356c
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2023-04-11 16:47:58 +0200

    Feature: Dash 2.9.x (#5)

--- backend/pages/second_page.py
+++ backend/pages/second_page.py
@@ -3,19 +3,32 @@ from dash import Input, Output, callback, dcc, html
 
 dash.register_page(__name__)
 
+initial_value = "Hello World!"
 
 layout = html.Div(
     [
         html.H3("Change the value in the text box to see callbacks in action!"),
         output_html := html.Div(),
-        html.Div(["Input: ", input_element := dcc.Input(value="initial value", type="text")]),
+        html.Div(["Input: ", input_element := dcc.Input(value=initial_value, type="text")]),
+        clear_button := html.Button("Clear"),
     ],
 )
 
 
 @callback(
-    Output(output_html, component_property="children"),
-    Input(input_element, component_property="value"),
+    Output(output_html, "children", allow_duplicate=True),
+    Input(input_element, "value"),
+    prevent_initial_call="initial_duplicate",
 )
 def update_output_div(input_value):
     return f"Output: {input_value}"
+
+
+@callback(
+    Output(output_html, "children", allow_duplicate=True),
+    Input(clear_button, "n_clicks"),
+    prevent_initial_call=True,
+)
+def clear_output(n_clicks):
+    if n_clicks:
+        return f"Output: {initial_value}"
